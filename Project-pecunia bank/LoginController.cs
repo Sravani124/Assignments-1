cfa5ec4d9755960574f1fb41190715e1c79393a2
Sravani124 <51781563+Sravani124@users.noreply.github.com>
2020-11-30 10:36:05 +0530

    Add files via upload

---
+++ Project-pecunia bank/LoginController.cs
@@ -0,0 +1,68 @@
+﻿using project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace project.Controllers
+{
+
+    public class LoginController : Controller
+    {
+        //PecuniaBankEntities db = new PecuniaBankEntities();
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Admin objchk)
+
+        {
+            if (ModelState.IsValid)
+            {
+                using (PecuniaBankEntities1 db = new PecuniaBankEntities1())
+                {
+                    var obj = db.Admins.Where(a => a.Username.Equals(objchk.Username) && a.Password.Equals(objchk.Password)).FirstOrDefault();
+
+                    if (obj != null)
+                    {
+
+                        Session["Username"] = obj.Username.ToString();
+
+                        Session["Password"] = obj.Password.ToString();
+                        //objchk.LoginErrorMsg = "Login Successfully.";
+
+                        //return View("Login", objchk);
+                        return RedirectToAction("Main", "Home");
+
+
+                    }
+                    else
+                    {
+
+                        ModelState.AddModelError("", "The Account_number or password is incorrect");
+                    }
+
+                }
+            }
+
+                    return View(objchk);
+                }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Home");
+        }
+
+       
+            }
+        }
+    
+    
+
+            
---
+++ Project-pecunia bank/RegisterTablesController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using project.Models;
+
+namespace project.Controllers
+{
+    public class RegisterTablesController : Controller
+    {
+        private PecuniaBankEntities db = new PecuniaBankEntities();
+
+        // GET: RegisterTables
+        public ActionResult Index()
+        {
+            return View(db.RegisterTables.ToList());
+        }
+
+        // GET: RegisterTables/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RegisterTable registerTable = db.RegisterTables.Find(id);
+            if (registerTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registerTable);
+        }
+
+        // GET: RegisterTables/Create
+        public ActionResult Create()
+        {
+            
+
+            return View();
+           
+        }
+
+        // POST: RegisterTables/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Account_Number,Account_Holdername,Email_id,Password,Confirm_Password,Mobile_Number,Address")] RegisterTable registerTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.RegisterTables.Add(registerTable);
+                db.SaveChanges();
+                TempData["SuccessMessage"] = "Account created Successfully";
+                return RedirectToAction("Index");
+            }
+           
+          
+
+
+            return View(registerTable);
+        }
+
+        // GET: RegisterTables/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RegisterTable registerTable = db.RegisterTables.Find(id);
+            if (registerTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registerTable);
+        }
+
+        // POST: RegisterTables/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Account_Number,Account_Holdername,Email_id,Password,Confirm_Password,Mobile_Number,Address")] RegisterTable registerTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(registerTable).State = EntityState.Modified;
+                db.SaveChanges();
+                TempData["SuccessMessage"] = "updated Successfully";
+                return RedirectToAction("Index");
+            }
+            return View(registerTable);
+        }
+
+        // GET: RegisterTables/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RegisterTable registerTable = db.RegisterTables.Find(id);
+            if (registerTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registerTable);
+        }
+
+        // POST: RegisterTables/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            RegisterTable registerTable = db.RegisterTables.Find(id);
+            db.RegisterTables.Remove(registerTable);
+          
+            db.SaveChanges();
+            //ViewBag.Message = "Are you sure to delete this record";
+            //return Content("<script language='javascript' type='text/javascript'>('Are you sure to delete this record.');" +
+            //    "window.location='Index';</script>");
+
+            TempData["SuccessMessage"] = "Deleted Successfully";
+            return RedirectToAction("Index");
+        }
+        
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
